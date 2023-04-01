eeb7fdf1be9c563d46b4aebf56c8f8e6369a3a07
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-04-01 12:37:18 +0200

    break

---
+++ src/BookList.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { Row } from 'react-bootstrap';
+
+
+class BookList extends Component {
+  state = {
+        books: []
+  }
+    
+
+    render()
+        return() {
+            return(
+                <>
+                <Row>
+                    <h1>The Newest and the Greatest</h1>
+                </Row>
+                <Row>
+                    {this.state.books.map(book => <img src={book.img} style={{width: "200px"}}/>)}
+                </Row>
+                </>
+            )
+        }
+}
+
+componentDidMount = async () => {
+    const resp = await fetch("./resources/scifi.json")
+    if(resp.ok){
+        const books = await resp.json()
+
+        .this.setState({
+            books: books 
+        })
+    }
+}
+
+export default BookList
